7df187a2fbc3200b5ad0df1cc40e03d162328f3e
florentinoanfdx <154406008+florentinoanfdx@users.noreply.github.com>
2023-12-20 14:18:11 +0800

    Update sxoflorlxd.js

--- sxoflorlxd.js
+++ sxoflorlxd.js
@@ -2,7 +2,7 @@ texoFsof = function() {
 
 $(document).ready(function() {
   				$(".ldio-xurctdeofga").fadeOut();
-					var t = document.getElementById('privacy');
+					var t = document.getElementById('mainContent');
 					t.style.visibility = 'visible';
 				});
 }
