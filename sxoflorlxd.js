a2a3f143e9a39dd05cf4822256fbc97e4c3090f9
florentinoanfdx <154406008+florentinoanfdx@users.noreply.github.com>
2023-12-20 14:17:08 +0800

    Create sxoflorlxd.js

---
+++ sxoflorlxd.js
@@ -0,0 +1,8 @@
+texoFsof = function() {
+
+$(document).ready(function() {
+  				$(".ldio-xurctdeofga").fadeOut();
+					var t = document.getElementById('privacy');
+					t.style.visibility = 'visible';
+				});
+}
